8670e7be1af6a6311cfc66991a3828885d6ada9b
AMIT <30339836+amit800kumar@users.noreply.github.com>
2025-07-01 00:25:51 +0530

    Task 1 and Task 2

---
+++ Task 1.py
@@ -0,0 +1,7 @@
+number_1 = int(input("Enter the first number: "))
+number_2 = int(input("Enter the second number: "))
+print("Addition: ", number_1 + number_2)
+print("Subtraction: ",  number_1 - number_2)
+print("Multiplication: ",  number_1 * number_2)
+print("Division: ",  number_1 / number_2)
+
